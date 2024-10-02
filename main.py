63ea06a79f5038f8dd05fa30ae520fcfcdd000ec
CalvinYrd <calvin59600@gmail.com>
2024-10-02 23:04:17 +0200

    commit

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-import os
+from subprocess import run as subprocess_run
+from os import name as os_name, system
+from colorama import Fore as color
 
-[os.system('cls') if os.name == "nt" else os.system('clear')]
+# permet de récupérer le résultat d'une commande
+def cmd(cmd):
+	return subprocess_run(
+		cmd, shell = True, capture_output = True, text = True, encoding="cp850"
+	).stdout
 
-if os.name == 'nt':
-	if os.popen('netsh wlan show profiles').read().split('\n')[9:-2]:
-		for network in os.popen('netsh wlan show profiles').read().split('\n')[9:-2]:
-			network = network.split(': ')[-1]
-			
-			try:
-				pwd = os.popen(f'netsh wlan show profiles name="{network}" key=clear').read().split('Contenu de la')[-1].split('    : ')[1].split('\n')[0]
+# effacement de l'écran
+if (os_name == "nt"): system("cls")
+else: system("clear")
 
-				print(f"--------------------------------------------------------------------------------------\nNom          : {network}\nMot de passe : {pwd}")
+# si le système est windows
+if (os_name == "nt"):
+	# récupération des réseaux wifi
+	networks = cmd("netsh wlan show profiles")
+	networks = networks.split("-----")[-1]
+	networks = networks.split("\n")[1:]
 
-			except IndexError:
-				pass
+	# affichage des infos de chaque wifi
+	for network in networks:
+		# on récupère uniquement le nom seul du wifi
+		network = network.split(":", 1)[-1].strip()
 
-		print("--------------------------------------------------------------------------------------\n\n\n\n")
+		# si le nom est vide (""), on ignore
+		if (network == ""): continue
 
-	else:
-		print("Aucun réseau(x) wifi trouvé(s), attention : il est possible qu'aucune carte wifi soit connectée à l'appareil.")
+		# récupération des informations du réseau
+		infos = cmd('netsh wlan show profiles name="' + network + '" key="clear"')
+		infos = infos.replace("Authentification", "", 1)
+		infos = infos.split("\n")
 
-else:
-	print("Désolé, votre système n'est pas supporté par ce programme.")
+		# affichage des informations prioritaires
+		for info in infos:
+			info = info.split(":", 1)
+
+			if (info[0].lower().strip() in ("nom du ssid", "contenu de la clé", "authentification")):
+				print(info[0].strip() + " : " + color.GREEN + info[1] + color.RESET)
+
+		print("\n" + color.LIGHTBLACK_EX + ("=" * 50) + color.RESET + "\n")
+
+# sinon, on indique que le programme n'est pas compatible
+else: print("Désolé, votre OS n'est pas compatible.")
